8686c5a20212c9c78346a7d50f17be29eb7eec8e
momo <294666880@qq.com>
2015-10-29 17:15:06 +0800

    Home模块总Model增加判断用户账号是否启用方法
    1、通过user_id直接判断
    2、通过s_id间接判断

--- Application/Home/Model/BaseModel.class.php
+++ Application/Home/Model/BaseModel.class.php
@@ -1,12 +1,30 @@
 <?php
 namespace Home\Model;
-
 use Common\Model\CommonModel;
 
-class BaseModel extends CommonModel
-{
+class BaseModel extends CommonModel{
     /**
      * Home模块总模型，直接继承CommonModel
      */
+    
+    /**
+     * 查询userId账号状态
+     * @param integer $userId 用户id
+     * $return integer 1为账号启用，0为账号被禁
+     */
+    protected function checkUserStatus($userId){
+        return M('user')->where('ur.`status`=1 AND user_id=%d',$userId)->count();
+    }
 
+    /**
+     * 查询shareId所属的用户账号状态
+     * @param integer $shareId 分享内容的id
+     * @return integer 1为账号启用，0为账号被禁
+     */
+    protected function checkUserStatus_byShareId($shareId){
+        return M('share')->alias('sh')
+                ->join('LEFT JOIN mn_user ur ON sh.user_id=ur.user_id')
+                ->where('ur.`status`=1 AND sh.s_id=%d',$shareId)
+                ->count();
+    }
 }
