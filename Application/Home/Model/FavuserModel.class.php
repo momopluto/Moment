1657af6bb013d81deda64e322301e1d790e74398
momo <294666880@qq.com>
2015-10-29 17:52:42 +0800

    完善FavuserModel
    1、无法收藏'被禁用的用户'
    2、允许取消收藏'被禁用的用户'

--- Application/Home/Model/FavuserModel.class.php
+++ Application/Home/Model/FavuserModel.class.php
@@ -1,72 +1,112 @@
 <?php
 namespace Home\Model;
-
 use Think\Model;
 
 /**
  * mn_favuser表模型
  */
-class FavuserModel extends BaseModel
-{
+class FavuserModel extends BaseModel{
 
     /**
      * 获取所有收藏的用户
-     * @return [type] [description]
+     * @param integer $ownerId 拥有者id
+     * @return string sql语句
      */
-    public function get_allfavusers()
-    {
+    public function getAllFavusers_sql($ownerId){
+        // 不管被收藏用户的账号是否启用，一律返回
+        // 允许取消收藏'被禁用的用户'
+        $sql = 'SELECT FROM_UNIXTIME(fu.cTime,"%Y-%m-%d %H:%i:%s") AS cTime, fu.user_id, ur.reg_time, ur.last_login_time, ur.`status`'
+                . ' FROM mn_favuser fu LEFT JOIN mn_user ur ON fu.user_id=ur.user_id'
+                . ' WHERE (fu.owner_id='.$ownerId.')'
+                . ' ORDER BY fu.cTime DESC';
+        return $sql;
     }
 
-    public function insertFavuser($ownerId, $userId)
-    {
-        $count = intval($this->checkExist($ownerId, $userId));
-        if($count === 0){
-            $result = $this->add([
-                'owner_id' => $ownerId,
-                'user_id'  => $userId,
-                'cTime'    => time(),
-            ]);
-
-            if($result === false){
-                $this->error = '收藏用户成功';
-
-                return false;
-            }
-
-            return true;
-        }else{
-            $this->error = '已收藏';
-
+    /**
+     * 插入收藏用户记录
+     * @param integer $ownerId 拥有者id
+     * @param integer $userId 被收藏者id
+     * @return boolean 另: 模型的error可获取错误提示信息
+     */
+    public function insertFavuser($ownerId, $userId){
+        // 不能收藏自己，ownerId != userId
+        if ($ownerId == $userId){
+            $err['errcode'] = 409;
+            $err['errmsg'] = "can't collect self";
+            $this->error = $err;
             return false;
         }
-    }
-
-    public function checkExist($ownerId, $userId)
-    {
-        return $this->where([
+        // 验证userId有效，账号启用
+        if (!$this->checkUserStatus($userId)){
+            $err['errcode'] = 412;
+            $err['errmsg'] = "target user was disabled";// userId账号状态为禁用
+            $this->error = $err;
+            return false;
+        }
+        // 验证ownerId和userId记录未存在
+        if ($this->where("owner_id = %d AND user_id = %d", array($ownerId, $userId))->count()) {
+            $err['errcode'] = 414;
+            $err['errmsg'] = "duplicated";
+            $this->error = $err;
+            return false;
+        }
+        
+        // $ownerId必合法，由Controller保证
+        $result = $this->add([
             'owner_id' => $ownerId,
             'user_id'  => $userId,
-        ])->count();
-    }
-
-    public function DelFavuser($ownerId, $userId)
-    {
-        $result = $this->where([
-            'owner_id' => $ownerId,
-            'user_id'  => $userId,
-        ])->delete();
+            'cTime'    => NOW_TIME,
+        ]);
 
         if($result === false){
-            $this->error = '取消收藏用户失败';
-
+            $err['errcode'] = 400;
+            $err['errmsg'] = "collect favuser failed";
+            $this->error = $err;
             return false;
         }
-
+        
+        $err['errcode'] = 0;
+        $err['errmsg'] = "ok";
+        $this->error = $err;
         return true;
     }
 
-    public function countFavuser($where)
-    {
-        return intval($this->where($where)->count());
+    /**
+     * 删除收藏用户记录
+     * @param integer $ownerId 拥有者id
+     * @param integer $userId 被收藏者id
+     * @return boolean 另: 模型的error可获取错误提示信息
+     */
+    public function delFavuser($ownerId, $userId){
+//        // 验证userId有效，账号启用
+//        // 允许取消收藏'被禁用的用户'
+//        if (!$this->checkUserStatus($userId)) {
+//            $err['errcode'] = 412;
+//            $err['errmsg'] = "target user was disabled"; // userId账号状态为禁用
+//            $this->error = $err;
+//            return false;
+//        }
+        // 验证$ownerId和$userId记录存在
+        $result = $this->where("owner_id = %d AND user_id = %d", array($ownerId, $userId))->delete();
+        if ($result === false) {
+            $err['errcode'] = 404;
+            $err['errmsg'] = "no match record";
+            $this->error = $err;
+            return false;
+        }
+
+        $err['errcode'] = 0;
+        $err['errmsg'] = "ok";
+        $this->error = $err;
+        return true;
+    }
+
+    /**
+     * [按条件]返回收藏用户的数目
+     * @param myx $map 条件
+     * @return integer 收藏用户数
+     */
+    public function countFavuser($map){
+        return $this->where($map)->count();
     }
 }
