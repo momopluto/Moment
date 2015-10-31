5df3ef0dd2b3c0e8f2c6d9f147b7b89a17ea6ecf
waiyungau <waiyungau@ishareh5.com>
2015-10-31 13:21:28 +0800

    controller 定义。

--- Application/Home/Controller/ComController.class.php
+++ Application/Home/Controller/ComController.class.php
@@ -1,23 +1,26 @@
 <?php
 namespace Home\Controller;
+
 use Think\Controller;
 
 /**
  * 通用控制器
  */
-class ComController extends BaseController {
+class ComController extends BaseController
+{
 
     /**
      * 最热的[若干条]分享
      * ps: 限制游客可浏览的分享数目
-     * 	   注册用户浏览需要分页
+     *       注册用户浏览需要分页
      * @return [type] [description]
      */
-    public function hotshare(){
+    public function hotshare()
+    {
         // 展示给游客最热的几条分享吧
         // 不然最新的分享相对来说变化较快
         // 而且最热的几条分享也能诱导用户注册
-        
+
         // 独立1个页面展示  ->游客
         // GET请求
     }
@@ -26,10 +29,11 @@ class ComController extends BaseController {
      * 查找分享
      * @return [type] [description]
      */
-    public function searchshare(){
+    public function searchshare()
+    {
         // 独立1个页面展示搜索结果
         // GET请求
-        
+
         // 限制搜索条件
         $q = I("param.wd", '', 'strip_tags');
         $page = intval(I('post.page', 1, 'strip_tags'));
@@ -46,11 +50,85 @@ class ComController extends BaseController {
      * 点赞榜
      * @return [type] [description]
      */
-    public function thumbuplist(){
+    public function thumbuplist()
+    {
         // 嵌入在主页中
         // GET请求 [AJAX]
-        
+
         // TODO，这应该写成1个model方法，用于获取数据即可
+    }
+
+    /**
+     * 个人主页
+     */
+    public function index()
+    {
+        // 某人的 id
+        $userId = I('param.id', '');
+    }
+
+    /**
+     * 个人相册
+     */
+    public function album()
+    {
+        $userId = I('param.id', '');
+    }
+
+    /**
+     * 粉丝
+     */
+    public function fans()
+    {
+        $userId = I('param.id', '');
+    }
+
+    /**
+     * 关注人
+     */
+    public function follow()
+    {
+        $userId = I('param.id', '');
+    }
+
+    /**
+     * 收藏列表
+     */
+    public function collection()
+    {
+        $userId = self::$user_id;
+    }
+
+    /**
+     * 收到的评论
+     */
+    public function commentReceive()
+    {
+        $userId = self::$user_id;
+    }
+
+    /**
+     * 发出的评论
+     */
+    public function commentSend()
+    {
+        $userId = self::$user_id;
+    }
+
+    /**
+     * 收到的点赞
+     */
+    public function thumbReceive()
+    {
+        $userId = self::$user_id;
+    }
+
+    /**
+     * 发出的点赞
+     */
+    public function thumbSend()
+    {
+        $userId = self::$user_id;
     }
 
 }
