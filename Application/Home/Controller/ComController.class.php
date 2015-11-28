a3ed41fcad8e993976a2a7cde62f91b4d1b4d067
waiyungau <waiyungau@ishareh5.com>
2015-11-28 11:13:26 +0800

    fix bug.

--- Application/Home/Controller/ComController.class.php
+++ Application/Home/Controller/ComController.class.php
@@ -172,9 +172,9 @@ class ComController extends BaseController
     {
         $userId = self::$user_id;
         $model = D('Thumb');
-        $sql = $model->getThumbReceive_count($userId);
+        $sql = $model->getThumbReceiveShare_sql($userId);
 
-        $count = $model->getThumbReceiveShare_sql($userId);// 查询满足要求的总记录数
+        $count = $model->getThumbReceive_count($userId);// 查询满足要求的总记录数
         $Page = new \Think\Page($count, 10);// 实例化分页类 传入总记录数和每页显示的记录数(10)
         $show = $Page->show();// 分页显示输出
         $sql .= ' limit ' . $Page->firstRow . ',' . $Page->listRows;// 拼装分页语句
