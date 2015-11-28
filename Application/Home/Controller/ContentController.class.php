8bc39f2b1b7b4d016fc51e2326c4c4d85948c121
waiyungau <waiyungau@ishareh5.com>
2015-11-28 11:26:50 +0800

    fix bug.

--- Application/Home/Controller/ContentController.class.php
+++ Application/Home/Controller/ContentController.class.php
@@ -52,7 +52,10 @@ class ContentController extends BaseController
         if(IS_POST){
             $text = I('post.text', '', 'strip_tags');
             $isPublic = I('post.is_public', '', 'strip_tags');
-
+            $fileCount = I('post.file_count');
+            if($fileCount !== count($_FILES)){
+                $this->dataReturn('100', '文件上传失败');
+            }
             $userId = self::$user_id;
             $model = D('content');
             $id = $model->insertShare($userId, $text, $isPublic);
